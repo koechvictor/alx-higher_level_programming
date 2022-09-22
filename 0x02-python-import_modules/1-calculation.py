30dd0098df251d275c85a57fad4c14968ff113a6
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-09-22 15:02:03 +0000

    task1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,8 @@
 #/usr/bin/python3
-from 1-calculation import add, sub, mul, div
+from calculator_1 import add, sub, mul, div
 if __name__ = "__main__"
+    a = 10
+    b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
