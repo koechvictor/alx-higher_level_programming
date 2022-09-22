9f3af197ece939ba1eac7c589583b13c228de331
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-09-22 14:48:20 +0000

    task1
    git commit -m task1
    
    git commit -m task1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,7 @@
+#/usr/bin/python3
+from 1-calculation import add, sub, mul, div
+if __name__ = "__main__"
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
