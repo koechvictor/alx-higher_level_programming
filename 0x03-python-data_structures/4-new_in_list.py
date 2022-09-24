08d8b5b2c26cee654af56bcf3f64db025ce904fd
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-09-24 14:38:38 +0000

    update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,6 +6,7 @@ elif idx > len(my_list) - 1:
     return my_list
 else:
     new_list = []
-    new_list.append(i)
+    for i in my_list:
+        new_list.append(i)
     new_list[idx] = element
-    return my_list
+    return new_list
