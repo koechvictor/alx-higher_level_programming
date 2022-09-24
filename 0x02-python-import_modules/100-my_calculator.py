cab8e572b3c10e62ff38f9758fe59bb3981bb350
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-09-24 12:46:42 +0000

    task6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,11 +2,11 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-if len(argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-    a = argv[1]
-    b = argv[3]
+    if len(argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
     op = argv[2]
     ops = {
             "+": add(a, b),
