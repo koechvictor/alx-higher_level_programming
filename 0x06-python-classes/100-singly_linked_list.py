14cdd6d967184f02f5e1939b454c9f0320a21e30
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-12 10:25:40 -0700

    task7

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,14 @@
+#!/usr/python3
+""" a class Node that defines a node of a singly linked list"""
+
+
+class Node:
+    """class SinglyLinkedList that defines a singly linked list """
+    def data(self):
+        pass
+    def next_node(self):
+        pass
+    def next_node(self, value):
+        pass
+    def __init__(self):
+        pass
