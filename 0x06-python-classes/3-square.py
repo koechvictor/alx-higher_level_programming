f69f98713b4bc85db2743e17fc19c7910d2d77a5
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-12 08:08:42 -0700

    task3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """based on 2-square.py"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
