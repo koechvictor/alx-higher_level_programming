6687cd07501c1d64cad38cf82f47dee769d8424e
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-12 10:29:05 -0700

    update

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,9 +1,5 @@
 #!/usr/python3
-""" a class Node that defines a node of a singly linked list"""
-
-
 class Node:
-    """class SinglyLinkedList that defines a singly linked list """
     def data(self):
         pass
     def next_node(self):
