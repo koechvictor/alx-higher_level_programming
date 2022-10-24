c16802a5bb2ce91d6dcea398aba8fa6cb60a3901
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-24 10:39:45 -0700

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
-Function thaat returns the list of available attributes
+Function that returns the list of available attributes
 and methods of an object
 """
 
 
 def lookup(obj):
+    """
+    Function that returns the list of available
+    and methods of an object
+    """
     return dir(obj)
