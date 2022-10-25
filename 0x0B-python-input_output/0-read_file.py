db185749090a010684536274414befcfb63b5686
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-25 01:55:20 -0700

    task1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a function that reads a text file  and prints it to stdout
+"""
+
+def read_file(filename=""):
+    """prints file contents"""
+    if filename == "":
+        return
+    with open(filename, "r") as f:
+        print(f.read(), end="")
