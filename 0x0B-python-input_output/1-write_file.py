4ebd9692048c04f8ddfb36330ed38be3045a9cdd
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-25 02:32:52 -0700

    task1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a function that writes a string to a text file
+and returns the number of characters written
+"""
+
+def write_file(filename="", text=""):
+    """count lines in a file"""
+    count = 0
+    if filename == "":
+        return count
+    with open(filename, "r") as f:
+        for l in f:
+            count += 1
+    return count
+
