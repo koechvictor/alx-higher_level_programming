d6eea67d0bbabc6c09f081c314b36d8187dd02fe
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-29 03:49:51 -0700

    update

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Rectangle that inherits Base """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Rectangle that inherits Base """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ returns width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ sets width value """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        returns height value
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        sets height value
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
