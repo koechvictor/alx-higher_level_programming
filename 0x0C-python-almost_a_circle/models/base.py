4fdb16c6d06af9ddcd76fb4bd91147140edcaa56
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-29 03:38:49 -0700

    update

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Base class """
+import json
+import turtle
+
+
+class Base:
+    """ Base class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is None:
+            type(self).__nb_objects += 1
+            self.id = self.__nb_objects
+        else:
+            self.id = id
