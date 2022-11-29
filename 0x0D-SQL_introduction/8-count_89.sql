b3d5123645d2ebd62bafb4b57df329de0680d315
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-11-29 09:29:28 -0800

    update

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- select all rows with id 89
-SELECT COUNT(*) FROM first_table WHERE id = 89;
+-- displays number of records with id=89 in first_table
+SELECT COUNT(*) FROM first_table WHERE `id`=89;
