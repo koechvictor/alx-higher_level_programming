1bc8da212bda55f5593904cf0d106a470d5b0869
victor koech <kipkiruiv408@gmail.com>
2023-01-17 09:16:24 +0000

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    res = requests.get("https://intranet.hbtn.io/status")
+    res = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type:", type(res.text))
     print("\t- content:", res.text)
